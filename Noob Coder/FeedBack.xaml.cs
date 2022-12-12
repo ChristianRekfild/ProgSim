7f7764dac9baddcb5d6f8ce484dbae9c582565e2
Николай Аврамов <nikavramov2015@gmail.com>
2022-12-12 18:41:23 +0300

    Добавьте файлы проекта.

---
+++ Noob Coder/FeedBack.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Noob_Coder
+{
+  /// <summary>
+  /// Логика взаимодействия для FeedBack.xaml
+  /// </summary>
+  public partial class FeedBack : Window
+  {
+    public FeedBack()
+    {
+      InitializeComponent();
+    }
+  }
+}
---
+++ Noob Coder/GameScene.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Noob_Coder
+{
+  public partial class GameScene : Window
+  {
+    public GameScene()
+    {
+      InitializeComponent();
+    }
+  }
+}
