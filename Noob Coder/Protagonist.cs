0d77a775bb7118bc42a3849c22ab24660cbd061a
BogartMod <bogartmod@gmail.com>
2022-12-14 13:24:47 +0300

    Create class Protogonist

---
+++ Noob Coder/Protagonist.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noob_Coder
+{
+    internal class Protagonist
+    {
+        float Health { get; set; }
+        BodyCondition Condition { get; set; }
+        HardSkills HardSkill { get; set; }
+        SoftSkills SoftSkill { get; set; }
+        decimal Money { get; set; }
+        decimal Credit { get; set; }
+        float Energy { get; set; }
+        float Appearance { get; set; }
+
+    }
+}
+
+enum BodyCondition
+{
+    None,
+    Cold,
+    Poising,
+    Hunger,
+    Fatigue,
+    Fracture
+}
+
+enum HardSkills
+{
+
+}
+enum SoftSkills
+{
+
+}
