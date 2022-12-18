81d5e30d6144baed601832096000ff80f052d195
a1Right <kostiantyns90@gmail.com>
2022-12-19 00:26:51 +0300

    Запуск хоста при старте приложения. Остановка хоста при остановке приложения и освобождение ресурсов хоста. Добавлен файл конфигурации приложения appsettings.json

--- Noob Coder/App.xaml.cs
+++ Noob Coder/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Noob_Coder.Infrastructure.Stores;
 using Noob_Coder.ViewModels;
 
@@ -9,8 +12,15 @@ namespace Noob_Coder
   /// </summary>
   public partial class App : Application
   {
-      protected override void OnStartup(StartupEventArgs e)
+      private static IHost _host;
+      public static IHost Host => _host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+      /// <summary>
+      /// Действия, выполняемые при старте приложения.
+      /// </summary>
+      /// <param name="e"></param>
+      protected override async void OnStartup(StartupEventArgs e)
       {
+          var host = Host;
           var navigationStore = new NavigationStore();
           navigationStore.CurrentViewModel = new MenuViewModel(navigationStore);
           MainWindow = new MainWindow()
@@ -19,6 +29,26 @@ namespace Noob_Coder
           };
           MainWindow.Show();
           base.OnStartup(e);
-        }
+          await host.StartAsync().ConfigureAwait(false);
+      }
+      /// <summary>
+      /// Действия, выполняемые при завершении работы приложения.
+      /// </summary>
+      /// <param name="e"></param>
+      protected override async void OnExit(ExitEventArgs e)
+      {
+          base.OnExit(e);
+          var host = Host;
+          await host.StopAsync().ConfigureAwait(false);
+          host.Dispose();
+      }
+
+      /// <summary>
+      /// Метод конфигурации DI контейнера внедрения зависимостей.
+      /// Здесь добавляются сервисы и их реализации.
+      /// </summary>
+      public static void ConfigureServices(HostBuilderContext hostBuilderContext, IServiceCollection services)
+      {
+      }
   }
 }
--- Noob Coder/Program.cs
+++ Noob Coder/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls.Ribbon.Primitives;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace Noob_Coder
 {
@@ -14,6 +18,22 @@ namespace Noob_Coder
             var app = new App();
             app.InitializeComponent();
             app.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var builder = Host.CreateDefaultBuilder(args);
+            var appsettingsPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "appsettings.json");
+            builder.UseContentRoot(AppDomain.CurrentDomain.BaseDirectory);
+            builder.ConfigureAppConfiguration((host, config) =>
+            {
+                config.SetBasePath(AppDomain.CurrentDomain.BaseDirectory);
+                config.AddJsonFile(appsettingsPath, optional:false, reloadOnChange:true);
+            });
+
+            builder.ConfigureServices(App.ConfigureServices);
+
+            return builder;
         }
     }
 }
