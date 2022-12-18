bfa8b041727b7d86a9e404099fcc4a038132f883
a1Right <kostiantyns90@gmail.com>
2022-12-18 23:09:07 +0300

    Описание конструктора RelayCommandBase.

--- Noob Coder/Infrastructure/Commands/RelayCommandBase.cs
+++ Noob Coder/Infrastructure/Commands/RelayCommandBase.cs
@@ -11,10 +11,11 @@ namespace Noob_Coder.Infrastructure.Commands
         private readonly Action<object> _execute;
         private readonly Func<object, bool> _canExecute;
         /// <summary>
-        /// Принимает два делегата Action для логики метода Execute(), Func<object, bool> для метода CanExecute (необязательный)
+        /// Принимает два делегата Action для логики метода Execute(), Func&lt;object, bool&gt; для метода CanExecute (необязательный)
         /// </summary>
-        /// <param name="execute">Action</param>
-        /// <param name="canExecute">Func</param>
+        /// <param name="execute">Метод описывает логику выполнения команды</param>
+        /// <param name="canExecute">Метод описывает логику возможности выполнения команды.
+        /// Необязательный параметр - если не передать CanExecute() будет всегда возвращать true</param>
         /// <exception cref="ArgumentNullException">Вызывает исключение, если передать null, вместо метода, описывающего логику выполнения команды</exception>
         public RelayCommandBase(Action<object> execute, Func<object, bool> canExecute = null)
         {
