46864a592f52e93e571f04e27cb925e98b571796
a1Right <kostiantyns90@gmail.com>
2022-12-18 23:33:05 +0300

    Изменена точка входа в приложение на Program.Main()

---
+++ Noob Coder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noob_Coder
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
