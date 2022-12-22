1416239dab87b5658ffc605cd7d0c45382d90c3f
Дмитрий Меднов <d.mednov@mail.ru>
2022-12-22 19:50:42 +0300

    Таймер

--- Noob Coder/ViewModels/GameSceneViewModel.cs
+++ Noob Coder/ViewModels/GameSceneViewModel.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
 using Noob_Coder.Infrastructure.Commands;
 using Noob_Coder.Infrastructure.Stores;
 using Noob_Coder.ViewModels.Base;
@@ -23,6 +27,22 @@ namespace Noob_Coder.ViewModels
             set => SetField(ref _title, value);
         }
 
+        private DateTime _gameDate = DateTime.Now;
+
+        public DateTime GameDate
+        {
+            get => _gameDate;
+            set => SetField(ref _gameDate, value);
+        }
+
+        private int _health = 100;
+
+        public int Health
+        {
+            get => _health;
+            set => SetField(ref _health, value);
+        }
+
         #endregion
 
         #region Команды
@@ -35,6 +55,25 @@ namespace Noob_Coder.ViewModels
         public GameSceneViewModel(NavigationStore navigationStore)
         {
             NavigateMenuCommand = new NavigateMenuCommand(navigationStore);
+            RunTimer().WaitAsync(_cts.Token);
+        }
+
+        private CancellationTokenSource _cts = new CancellationTokenSource();
+
+        public void CancelTimer()
+        {
+            _cts.Cancel();
+        }
+
+        private async Task RunTimer()
+        {
+            while (!_cts.IsCancellationRequested)
+            {
+                Task.Yield();
+                await Task.Delay(1000);
+                GameDate = _gameDate.AddDays(1);
+                Health = _health - 1;
+            }
         }
     }
 }
