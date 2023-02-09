e040801f9ee5433128114fedb5d4142705b5ca34
BogartMod <bogartmod@gmail.com>
2023-02-09 19:30:57 +0300

    Наборок методов банка

--- Noob Coder/Models/Bank.cs
+++ Noob Coder/Models/Bank.cs
@@ -1,25 +1,106 @@
 ﻿namespace Noob_Coder.Models
 {
+    /// <summary>
+    /// Класс банк хранит значения счета клиента.
+    /// Выдает кредит
+    /// Гасит кредит
+    /// Принимает деньги на депозит
+    /// Выдает деньги с депозита
+    /// </summary>
     internal class Bank
     {
         private int _balance;
+        /// <param>
+        /// Баланс на банковском счету
+        /// Отрицательный - кредит
+        /// Положительный - депозит
+        /// </param>
+        public int Balance { get { return _balance; } }
+
         private int _maximumCredit;
 
-        public int GetDebt(int debt)
+        /// <summary>
+        /// Взять в кредит (колво-денег).
+        /// </summary>
+        /// <param name="takedMoney">Сколько денег хочет взять</param>
+        public int GetDebt(int takedMoney) 
         {
-            if (_balance - debt < _maximumCredit)
+            if (_balance - takedMoney < _maximumCredit)
             {
-                _balance -= debt;
+                int debt = _maximumCredit + takedMoney;
+                _balance -= -debt;
+                return takedMoney;
+            }
+            else if (_balance == _maximumCredit)
+                return 0;
+            else
+            {
+                _balance -= takedMoney;
+                return takedMoney;
+            }
+            
+        }
+
+        /// <summary>
+        /// Погасить кредит на сумму.
+        /// </summary>
+        /// <param name="takedMoney">Сколько денег хочет взять</param>
+        public int PayOffTheDebt(int payment)
+        {
+            if((payment > _balance)&&(_balance<=0))
+            {
+                int debt = -_balance;
+                _balance = 0;
+                return debt;
             }
             else
             {
-                _balance = _maximumCredit;
+                return 0;
             }
+            
         }
 
-        public int PayOffTheDebt(int payment)
+
+        /// <summary>
+        /// Обналичить депозит
+        /// </summary>
+        /// <param name="takedMoney">Сколько денег хочет взять</param>
+        public int CashOut(int takedMoney)
         {
-            _balance += payment;
+            if(takedMoney > _balance)
+            {
+                _balance = 0; 
+            }
+            _balance += takedMoney;
+            return 0;
+        }
+
+        /// <summary>
+        /// Положить деньги в банк под проценты
+        /// </summary>
+        /// <param name="takedMoney">Сколько денег хочет взять</param>
+        public int DepositMoney(int takedMoney)
+        {
+            if (takedMoney > _balance)
+            {
+                _balance = 0;
+            }
+            _balance += takedMoney;
+            return 0;
+        }
+
+        /// <summary>
+        /// Пересчитать проценты ежедневные.
+        /// </summary>
+        /// <param name="takedMoney">Сколько денег хочет взять</param>
+        public int RecalculateBankInterest(int takedMoney)
+        {
+            if (takedMoney > _balance)
+            {
+                _balance = 0;
+            }
+            _balance += takedMoney;
+            return 0;
         }
     }
 }
