4e64aaff5e7f12ba4e7505a986b1dea0b8ba5ccc
BogartMod <bogartmod@gmail.com>
2023-02-09 14:31:24 +0300

    Создание класса банк

--- Noob Coder/Models/Protagonist.cs
+++ Noob Coder/Models/Protagonist.cs
@@ -114,4 +114,27 @@ namespace Noob_Coder.Models
 
 
     }
+
+    internal class Bank
+    {
+        private int _balance;
+        private int _maximumCredit;
+
+        public int GetDebt(int debt)
+        {
+            if (_balance - debt < _maximumCredit)
+            {
+                _balance -= debt;
+            }
+            else
+            {
+                _balance = _maximumCredit;
+            }
+        }
+
+        public int PayOffTheDebt(int payment)
+        {
+            _balance += payment;
+        }
+    }
 }
