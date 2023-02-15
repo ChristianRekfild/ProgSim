d005213f72f327b52f119047fbfca21f33730c58
Ratmir Kremcheev <ratmir.kremcheev@gmail.com>
2023-02-15 16:34:16 +0300

    cleanup

--- Noob Coder/App.xaml.cs
+++ Noob Coder/App.xaml.cs
@@ -6,7 +6,6 @@ using Noob_Coder.Handlers;
 using Noob_Coder.Infrastructure.Stores;
 using Noob_Coder.Models;
 using Noob_Coder.Services;
-using Noob_Coder.UserInterface;
 using Noob_Coder.ViewModels;
 
 namespace Noob_Coder
@@ -18,36 +17,38 @@ namespace Noob_Coder
   {
       private static IHost _host;
       public static IHost Host => _host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
       /// <summary>
       ///Метод возвращает путь к папке, в которой запущено приложение.
       /// </summary>
       /// <returns>Путь к текущей папке из которого запущено приложение в формате string </returns>
       public static string CurrentAppRunningDirectory() => AppDomain.CurrentDomain.BaseDirectory;
+
       /// <summary>
       /// Действия, выполняемые при старте приложения.
       /// </summary>
       /// <param name="e"></param>
       protected override async void OnStartup(StartupEventArgs e)
       {
-          var host = Host;
-
-          SettingsSaveModel settingsSaveModel = new SettingsSaveModel();
+          var settingsSaveModel = new SettingsSaveModel();
           settingsSaveModel.Load();
-          var navigationStore = host.Services.GetService<NavigationStore>();
-          navigationStore.CurrentViewModel = host.Services.GetRequiredService<MenuViewModel>();
-          MainWindow = _host.Services.GetService<MainWindow>();
+
+          var navigationStore = Host.Services.GetRequiredService<NavigationStore>();
+          navigationStore.CurrentViewModel = Host.Services.GetRequiredService<MenuViewModel>();
+          MainWindow = Host.Services.GetRequiredService<MainWindow>();
           MainWindow.Show();
-          base.OnStartup(e);
-          await host.StartAsync().ConfigureAwait(false);
 
           AddHandlers();
+
+          base.OnStartup(e);
+          await Host.StartAsync().ConfigureAwait(false);
       }
 
       private static void AddHandlers()
       {
           var backgroundService = Host.Services.GetRequiredService<GameBackgroundService>();
 
-          foreach (var handler in _host.Services.GetServices<IHandler>())
+          foreach (var handler in Host.Services.GetServices<IHandler>())
           {
               backgroundService.DayChanged += handler.HandleNewDayEvent;
           }
